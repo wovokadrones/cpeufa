767dddaeeb8fc953445280dff2e907c42204b36a
krquinton <134304945+wovokadrones@users.noreply.github.com>
2023-06-21 16:57:12 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,635 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cpeufa-1",
+      "name": "CPEUFA 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.9407355862548759,
+        "pitch": 0.48770557223387456,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.72399004032615,
+          "pitch": 0.09212598234046254,
+          "rotation": 0,
+          "target": "1-cpeufa-2"
+        },
+        {
+          "yaw": -1.2709627592706774,
+          "pitch": -0.11031034775533577,
+          "rotation": 0,
+          "target": "2-cpeufa-3"
+        },
+        {
+          "yaw": -0.8647288552714922,
+          "pitch": -0.09459503364825395,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": 1.2000799740347148,
+          "pitch": -0.02923561735675051,
+          "rotation": 0,
+          "target": "6-cpeufa-7"
+        },
+        {
+          "yaw": 0.8190695350776327,
+          "pitch": 0.16222170235880107,
+          "rotation": 0,
+          "target": "7-cpeufa-8"
+        },
+        {
+          "yaw": -0.4810230259348707,
+          "pitch": -0.10236331038759516,
+          "rotation": 0,
+          "target": "4-cpeufa-5"
+        },
+        {
+          "yaw": -0.06780550970931642,
+          "pitch": -0.04518622093958058,
+          "rotation": 0,
+          "target": "5-cpeufa-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cpeufa-2",
+      "name": "CPEUFA 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0246853954287882,
+          "pitch": 0.1611896290912025,
+          "rotation": 0,
+          "target": "0-cpeufa-1"
+        },
+        {
+          "yaw": -0.35533505234632123,
+          "pitch": -0.12391236105984227,
+          "rotation": 0,
+          "target": "2-cpeufa-3"
+        },
+        {
+          "yaw": 0.9295554333265628,
+          "pitch": -0.11471199381995234,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": 0.14175016481656044,
+          "pitch": -0.13363860956885887,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": 0.6611725167133073,
+          "pitch": -0.1691374218423789,
+          "rotation": 0,
+          "target": "4-cpeufa-5"
+        },
+        {
+          "yaw": 2.1049365821260224,
+          "pitch": 0.30337055655672174,
+          "rotation": 0,
+          "target": "7-cpeufa-8"
+        },
+        {
+          "yaw": 2.333748073752309,
+          "pitch": -0.019398519754066257,
+          "rotation": 0,
+          "target": "6-cpeufa-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cpeufa-3",
+      "name": "CPEUFA 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -2.202233028774403,
+        "pitch": 0.298290011347472,
+        "fov": 1.4385746071217929
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7174746612879357,
+          "pitch": -0.06504578794923077,
+          "rotation": 0,
+          "target": "1-cpeufa-2"
+        },
+        {
+          "yaw": -2.8850281024477855,
+          "pitch": 0.03047581667452981,
+          "rotation": 0,
+          "target": "0-cpeufa-1"
+        },
+        {
+          "yaw": 0.5192646612653746,
+          "pitch": -0.15026189131570433,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": 1.2892244768603156,
+          "pitch": -0.21883303444057844,
+          "rotation": 0,
+          "target": "4-cpeufa-5"
+        },
+        {
+          "yaw": 2.1688822850788547,
+          "pitch": -0.038902376599240895,
+          "rotation": 0,
+          "target": "5-cpeufa-6"
+        },
+        {
+          "yaw": 2.765080038397665,
+          "pitch": -0.020266205103311563,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": 3.101922731911557,
+          "pitch": 0.10757508184110165,
+          "rotation": 0,
+          "target": "7-cpeufa-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cpeufa-4",
+      "name": "CPEUFA 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5060944802535747,
+          "pitch": 0.005328164398424207,
+          "rotation": 0,
+          "target": "0-cpeufa-1"
+        },
+        {
+          "yaw": -1.8611848739521193,
+          "pitch": 0.07633826566823743,
+          "rotation": 0,
+          "target": "1-cpeufa-2"
+        },
+        {
+          "yaw": -0.9801948997344354,
+          "pitch": 0.03544140079019087,
+          "rotation": 0,
+          "target": "2-cpeufa-3"
+        },
+        {
+          "yaw": 1.5350684771546579,
+          "pitch": 0.021179381761742277,
+          "rotation": 0,
+          "target": "4-cpeufa-5"
+        },
+        {
+          "yaw": 2.21133783155833,
+          "pitch": -0.03987587140051119,
+          "rotation": 0,
+          "target": "5-cpeufa-6"
+        },
+        {
+          "yaw": 2.7162562351457717,
+          "pitch": 0.004343362693692399,
+          "rotation": 0,
+          "target": "6-cpeufa-7"
+        },
+        {
+          "yaw": -3.0916764626265874,
+          "pitch": 0.0370582898305738,
+          "rotation": 0,
+          "target": "7-cpeufa-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cpeufa-5",
+      "name": "CPEUFA 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6042571625073503,
+          "pitch": -0.014884502069461902,
+          "rotation": 0,
+          "target": "0-cpeufa-1"
+        },
+        {
+          "yaw": -1.0608984950802398,
+          "pitch": -0.004837723968556418,
+          "rotation": 0,
+          "target": "1-cpeufa-2"
+        },
+        {
+          "yaw": -0.47429236117040574,
+          "pitch": 0.012757987940975823,
+          "rotation": 0,
+          "target": "2-cpeufa-3"
+        },
+        {
+          "yaw": 0.22663629282169317,
+          "pitch": 0.0851415979146708,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": 2.9786762483018947,
+          "pitch": 0.021663279449427364,
+          "rotation": 0,
+          "target": "5-cpeufa-6"
+        },
+        {
+          "yaw": -2.9069266439271004,
+          "pitch": -0.04677873404028965,
+          "rotation": 0,
+          "target": "6-cpeufa-7"
+        },
+        {
+          "yaw": -2.107671017843277,
+          "pitch": -0.0037634390893810377,
+          "rotation": 0,
+          "target": "7-cpeufa-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-cpeufa-6",
+      "name": "CPEUFA 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8999485984603162,
+          "pitch": -0.03286618805226027,
+          "rotation": 0,
+          "target": "0-cpeufa-1"
+        },
+        {
+          "yaw": -0.07411308584389786,
+          "pitch": -0.0406730655944898,
+          "rotation": 0,
+          "target": "1-cpeufa-2"
+        },
+        {
+          "yaw": 0.47122780160267297,
+          "pitch": -0.05735867693515928,
+          "rotation": 0,
+          "target": "2-cpeufa-3"
+        },
+        {
+          "yaw": 1.49614503966429,
+          "pitch": -0.14814004780795287,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": 2.3863734992223042,
+          "pitch": -0.1612634254400369,
+          "rotation": 0,
+          "target": "4-cpeufa-5"
+        },
+        {
+          "yaw": -2.4930253533064395,
+          "pitch": -0.0821540254413371,
+          "rotation": 0,
+          "target": "6-cpeufa-7"
+        },
+        {
+          "yaw": -1.8645744407466562,
+          "pitch": 0.07117114572043626,
+          "rotation": 0,
+          "target": "7-cpeufa-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-cpeufa-7",
+      "name": "CPEUFA 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10320977436602519,
+          "pitch": -0.01602498762817639,
+          "rotation": 0,
+          "target": "0-cpeufa-1"
+        },
+        {
+          "yaw": 0.7927326667266037,
+          "pitch": 0.0059607245981609935,
+          "rotation": 0,
+          "target": "1-cpeufa-2"
+        },
+        {
+          "yaw": 1.7671928548591929,
+          "pitch": -0.02892558202544926,
+          "rotation": 0,
+          "target": "2-cpeufa-3"
+        },
+        {
+          "yaw": 2.4412690030492463,
+          "pitch": -0.11429700186097946,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": 2.977629331511217,
+          "pitch": -0.141184133218621,
+          "rotation": 0,
+          "target": "4-cpeufa-5"
+        },
+        {
+          "yaw": -2.858674142214504,
+          "pitch": -0.1022960772036825,
+          "rotation": 0,
+          "target": "5-cpeufa-6"
+        },
+        {
+          "yaw": 0.3878529832348043,
+          "pitch": 0.30013727698817405,
+          "rotation": 0,
+          "target": "7-cpeufa-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-cpeufa-8",
+      "name": "CPEUFA 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6421283990234148,
+          "pitch": -0.0715993339339498,
+          "rotation": 0,
+          "target": "0-cpeufa-1"
+        },
+        {
+          "yaw": 1.7342096072483777,
+          "pitch": -0.08003559120355774,
+          "rotation": 0,
+          "target": "1-cpeufa-2"
+        },
+        {
+          "yaw": 2.903561851047834,
+          "pitch": -0.11649345444351944,
+          "rotation": 0,
+          "target": "2-cpeufa-3"
+        },
+        {
+          "yaw": -2.35185372943638,
+          "pitch": -0.23277759210626314,
+          "rotation": 0,
+          "target": "3-cpeufa-4"
+        },
+        {
+          "yaw": -2.1378142469768147,
+          "pitch": -0.22784507059587256,
+          "rotation": 0,
+          "target": "4-cpeufa-5"
+        },
+        {
+          "yaw": -1.7053218565562513,
+          "pitch": -0.21923110497660758,
+          "rotation": 0,
+          "target": "5-cpeufa-6"
+        },
+        {
+          "yaw": -0.8788010224645255,
+          "pitch": -0.1410636470090072,
+          "rotation": 0,
+          "target": "6-cpeufa-7"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "CPEUFA - San Pascual",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
